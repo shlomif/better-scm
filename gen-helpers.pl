22bf3611ed88301110b15d52d30615fa930b53ee
shlomif <shlomif@38b9c29c-9acb-40d3-b369-b575a8dbbeea>
2005-04-23 21:57:07 +0000

    Ported gen-helpers.pl and the Makefile to GenMakeHelpers.

--- gen-helpers.pl
+++ gen-helpers.pl
@@ -3,70 +3,23 @@
 use strict;
 use warnings;
 
-use File::Find::Rule;
-use File::Basename;
+use HTML::Latemp::GenMakeHelpers;
 
-my $dir = "src";
-my @files = File::Find::Rule->in($dir);
-
-s!^src/!! for @files;
-@files = (grep { $_ ne $dir } @files);
-@files = (grep { ! m{(^|/)\.svn(/|$)} } @files);
-@files = (grep { ! /~$/ } @files);
-@files = 
-    (grep 
-    {
-        my $b = basename($_); 
-        !(($b =~ /^\./) && ($b =~ /\.swp$/))
-    } 
-    @files
+my $generator =
+    HTML::Latemp::GenMakeHelpers->new(
+        'hosts' =>
+        [
+            {
+                'id' => "berlios",
+                'source_dir' => "src",
+                'dest_dir' => "\$(D)",
+            },
+            {
+                'id' => "common",
+                'source_dir' => "common",
+                'dest_dir' => "\$(D)",
+            },
+        ],
     );
-@files = sort { $a cmp $b } @files;
 
-my @buckets = 
-(
-    {
-        'name' => "IMAGES_PRE1",
-        'filter' => sub { (!/\.wml$/) && (-f "$dir/$_") },
-    },
-    {
-        'name' => "SUBDIRS_PROTO",
-        'filter' => sub { (-d "$dir/$_") },
-    },
-    {
-        'name' => "HTMLS_PROTO",
-        'filter' => sub { /\.html\.wml$/ },
-        'map' => sub { my $a = shift; $a =~ s{\.wml$}{}; return $a;},
-    },
-);
-
-foreach (@buckets) 
-{ 
-    $_->{'results'}=[]; 
-    if (!exists($_->{'map'}))
-    {
-        $_->{'map'} = sub { return shift;},
-    }
-}
-
-FILE_LOOP: foreach (@files)
-{
-    for my $b (@buckets)
-    {
-        if ($b->{'filter'}->())
-        {
-            push @{$b->{'results'}}, $b->{'map'}->($_);
-            next FILE_LOOP;
-        }
-    }
-    die "Uncategorized file $_!";
-}
-
-open O, ">", "include.mak";
-foreach my $b (@buckets)
-{
-    print O $b->{'name'} . " = " . join(" ", @{$b->{'results'}}) . "\n";
-}
-close(O);
-
-1;
+$generator->process_all();
