0a1e47c85b72a7159f82c908e023ff59e6467f64
shlomif <shlomif@38b9c29c-9acb-40d3-b369-b575a8dbbeea>
2005-05-03 18:25:58 +0000

    Generalized gen-feeds.pl and the makefile.

--- gen-feeds.pl
+++ gen-feeds.pl
@@ -4,11 +4,15 @@ use strict;
 use warnings;
 
 use MyManageNews;
+use Getopt::Long;
+
+my $rss2_out = "dest/rss.xml";
+GetOptions ("rss2-out=s" => \$rss2_out);
 
 my $news_manager = get_news_manager();
 
 $news_manager->generate_rss_feed(
-    'output_filename' => "dest/rss.xml"
+    'output_filename' => $rss2_out,
 );
 
 1;
