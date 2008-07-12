e610cc6ad4a0284252e7c4a86e196e2b5d45157d
shlomif <shlomif@38b9c29c-9acb-40d3-b369-b575a8dbbeea>
2008-07-12 07:25:40 +0000

    Added another item.

--- src/comparison/render.pl
+++ src/comparison/render.pl
@@ -4,17 +4,11 @@ use strict;
 
 use XML::CompareML::HTML;
 
-open O, ">comparison.html";
-
-my $converter = 
-    XML::CompareML::HTML->new(
-        'input_filename' => "./scm-comparison.xml",
-        'output_handle' => \*O,
-    );
-
-$converter->process();
-
-close(O);
+system("xsltproc", 
+    "--output", "comparison.html",
+    "compare-ml.xsl",
+    "scm-comparison.xml",
+);
 
 1;
 
