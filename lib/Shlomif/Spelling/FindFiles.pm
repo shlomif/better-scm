65ec52384bac818435fcbd2071e46d88f0889176
Shlomi Fish <shlomif@shlomifish.org>
2016-11-02 17:53:04 +0200

    Cleanup the copy+pasta spell checking files.

--- lib/Shlomif/Spelling/FindFiles.pm
+++ lib/Shlomif/Spelling/FindFiles.pm
@@ -10,13 +10,6 @@ use HTML::Spelling::Site::Finder;
 
 my @prunes =
 (
-    qr#\Adest/tutorials/modern-perl/#,
-    qr#\Adest/tutorials/hyperpolyglot/#,
-    qr#\Adest/tutorials/impatient-perl/iperl\.html#,
-    qr#\Adest/topics/cpan/life-with-cpan/#,
-    qr#\Adest/humour/#,
-    qr#\Adest/FAQs/freenode-perl/#,
-    qr#\Adest/uses/text-parsing/htmlparsing.icenina.ca/#,
 );
 
 sub list_htmls
