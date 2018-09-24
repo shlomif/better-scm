12748936fccb3b9c48ccfa105e48572d481ae3db
Shlomi Fish <shlomif@shlomifish.org>
2018-09-24 16:16:34 +0300

    Fix make/etc. dependencies resolution issues.

---
+++ bin/filter-make.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+exec( 'zenfilter', '--count-step=200', '--last=500' );
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2016 by Shlomi Fish
+
+This program is distributed under the MIT (Expat) License:
+L<http://www.opensource.org/licenses/mit-license.php>
+
+=cut
