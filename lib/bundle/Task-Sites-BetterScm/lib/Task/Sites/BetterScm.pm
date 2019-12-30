fccab5213fb67a069892826eafc720d423c097f8
Shlomi Fish <shlomif@shlomifish.org>
2019-12-30 13:45:11 +0200

    add a script

--- lib/bundle/Task-Sites-BetterScm/lib/Task/Sites/BetterScm.pm
+++ lib/bundle/Task-Sites-BetterScm/lib/Task/Sites/BetterScm.pm
@@ -12,39 +12,171 @@ Task::Sites::BetterScm - Specifications for modules needed by the better scm sit
 
 =pkg App::Deps::Verify
 
+=pkg App::Gezer
+
 =pkg App::XML::DocBook::Builder
 
 =pkg Bit::Vector
+
+=pkg CGI
+
+=pkg Carp
 
 =pkg Carp::Always
 
 =pkg Class::XSAccessor
 
-=pkg Class::XSAccessor
+=pkg Code::TidyAll::Plugin::TestCount
 
 =pkg Config::IniFiles
+
+=pkg Cwd
+
+=pkg Data::Dumper
+
+=pkg Data::Munge
+
+=pkg Encode
+
+=pkg English
+
+=pkg Exporter
+
+=pkg Fcntl
+
+=pkg File::Basename
+
+=pkg File::Copy
+
+=pkg File::Find
+
+=pkg File::Find::Object
+
+=pkg File::Find::Object::Rule
+
+=pkg File::Path
+
+=pkg File::Spec
+
+=pkg File::Spec::Functions
+
+=pkg File::Update
+
+=pkg File::Which
 
 =pkg GD
 
 =pkg Getopt::Long
 
+=pkg HTML::Latemp::GenMakeHelpers
+
+=pkg HTML::Latemp::NavLinks::GenHtml::ArrowImages
+
+=pkg HTML::Latemp::News
+
 =pkg HTML::Links::Localize
+
+=pkg HTML::Parser
+
+=pkg HTML::Spelling::Site
 
 =pkg HTML::T5
 
+=pkg HTML::TokeParser::Simple
+
+=pkg HTML::TreeBuilder::LibXML
+
+=pkg HTML::Widgets::NavMenu
+
+=pkg HTML::Widgets::NavMenu::EscapeHtml
+
+=pkg HTML::Widgets::NavMenu::ToJSON
+
+=pkg HTML::Widgets::NavMenu::ToJSON::Data_Persistence::YAML
+
 =pkg IO::All
+
+=pkg IO::Handle
 
 =pkg Image::Size
 
+=pkg JSON::MaybeXS
+
 =pkg List::MoreUtils
 
+=pkg List::Util
+
+=pkg POSIX
+
+=pkg Parallel::ForkManager::Segmented
+
 =pkg Path::Tiny
+
+=pkg Perl::Tidy
 
 =pkg Pod::Xhtml
 
 =pkg Task::Latemp
 
+=pkg Template
+
+=pkg Template::Preprocessor::TTML
+
 =pkg Term::ReadKey
+
+=pkg Test::Code::TidyAll
+
+=pkg Test::File::Find::CaseCollide
+
+=pkg Test::HTML::Recursive::DeprecatedTags
+
+=pkg Test::HTML::Tidy::Recursive
+
+=pkg Test::HTML::Tidy::Recursive::Strict
+
+=pkg Test::More
+
+=pkg Test::Perl::Critic
+
+=pkg Test::TrailingSpace
+
+=pkg Text::Hunspell
+
+=pkg Text::VimColor
+
+=pkg Text::WrapAsUtf8
+
+=pkg XML::CompareML::HTML
+
+=pkg XML::LibXML
+
+=pkg XML::LibXML::Reader
+
+=pkg XML::LibXML::XPathContext
+
+=pkg XML::LibXSLT
+
+=pkg XML::Parser
+
+=pkg XML::Writer
+
+=pkg autodie
+
+=pkg base
+
+=pkg constant
+
+=pkg lib
+
+=pkg parent
+
+=pkg strict
+
+=pkg utf8
+
+=pkg vars
+
+=pkg warnings
 
 =head1 DESCRIPTION
 
