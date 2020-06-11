e9e6db8415ad8f4722c6fb54f71ed93e447e910d
Shlomi Fish <shlomif@shlomifish.org>
2020-06-11 17:55:44 +0300

    convert wml->tt2: cvt docs/* pages

--- bin/tot.bash
+++ bin/tot.bash
@@ -4,7 +4,7 @@ mkdir -p "$dir"
 adest="$dir/have.xhtml"
 bdest="$dir/want.xhtml"
 rm -f "$adest" "$bdest"
-for a in "dest/alternatives/darcs/index.html"
+for a in "dest/contribute/index.html"
 do
 b=/home/shlomif/Backup/Arcs/better-scm-post-dest/post-incs/"${a##dest/}"
 f()
